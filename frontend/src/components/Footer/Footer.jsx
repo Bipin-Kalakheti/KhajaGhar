9b7aec9d66e225a10f4e46563a23ca4658784131
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-11 16:43:21 -0400

    Implement footer component, food filter by category

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import { assets } from "../../assets/assets";
+import "./footer.css";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus
+            ratione voluptatem assumenda neque quis eveniet, error quibusdam
+            aliquid cupiditate quia{" "}
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-416-7809</li>
+            <li>contactus@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright {currentYear} - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
