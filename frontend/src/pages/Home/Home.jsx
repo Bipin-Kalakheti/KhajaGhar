6a54d7bc892d092c662d9259b5ec149675a6affd
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-11 14:31:33 -0400

    implement header, home, explore_menu component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-import './home.css'
+import { useState } from "react";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import Header from "../../components/Header/Header";
+import "./home.css";
 
 const Home = () => {
-  return (
-    <div className='home'>Home</div>
-  )
-}
+  const [category, setCategory] = useState("All");
 
-export default Home
+  return (
+    <div className="home">
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  );
+};
+
+export default Home;
